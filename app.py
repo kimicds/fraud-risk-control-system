d85bb2abe0ddc015eba081fb1760f0b7b6b5e71a
Odunola Akimu Mosebolatan <146633788+kimicds@users.noreply.github.com>
2026-03-11 13:39:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -242,9 +242,8 @@ def normal_records():
 
 if __name__ == "__main__":
 
-    #app.run(debug=True)
-    port = int(os.environ.get("PORT", 5000))  # Render provides PORT env variable
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
+
 
 
 
