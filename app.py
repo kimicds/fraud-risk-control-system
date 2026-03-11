96746bb007f55f7495f0cd6fe31c2488ac17445f
Odunola Akimu Mosebolatan <146633788+kimicds@users.noreply.github.com>
2026-03-11 11:53:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -241,4 +241,7 @@ def normal_records():
     return render_template("normal_records.html", data=records)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+
+    #app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render provides PORT env variable
+    app.run(host="0.0.0.0", port=port)
